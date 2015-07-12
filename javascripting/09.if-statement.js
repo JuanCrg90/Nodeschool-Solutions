c0417271c7c13c82648019f346929aa9488d0493
Juan Carlos Ruiz <juancarlosruiz14@gmail.com>
2015-07-11 19:34:42 -0500

    Added The javascripting solutions

---
+++ javascripting/09.if-statement.js
@@ -0,0 +1,7 @@
+var fruit = 'orange';
+if(fruit.length > 5){
+	console.log('The fruit name has more than five characters.');
+}
+else{
+	console.log('Te fruit name has five characters or less');
+}
---
+++ javascripting/18.function-arguments.js
@@ -0,0 +1,6 @@
+function math(f,s,t){
+	var mult = s * t;
+	return mult + f;
+}
+
+console.log(math(53,61,67));
