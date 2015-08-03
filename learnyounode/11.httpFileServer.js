e989cc35757bad20ce6d2bd4821f736a9c90b013
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2015-08-03 18:34:14 -0500

    added exercise 11

---
+++ learnyounode/11.httpFileServer.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+var port = process.argv[2];
+var file = process.argv[3];
+console.log();
+var server = http.createServer(function(req,res){
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  fs.createReadStream(file).pipe(res);
+
+});
+
+server.listen(port);
